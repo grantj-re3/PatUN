cb57e10f273e0383ccfb373f5db676486b570f18
grantj-re3 <grantj-re3@github.fake>
2020-08-08 19:58:01 +0930

    Update so that most commands do not require user to press ENTER

--- patun_event.rb
+++ patun_event.rb
@@ -8,7 +8,11 @@
 # http://www.gnu.org/licenses/
 #
 ##############################################################################
+require 'io/console'
+
 class PatUnEvent
+  WITHOUT_ENTER_KEY = true	# Most commands do not require user to press ENTER
+
   # Main-menu keyboard inputs
   S_DOWN	= "4"	# Numeric keypad: left arrow
   S_UP		= "6"	# Numeric keypad: right arrow
@@ -44,7 +48,7 @@ class PatUnEvent
   def get_more
     while true
       printf "Enter command (%s=Game ID, %s=Show Rules, %s=Continue): ", S_GAME_ID, S_RULES, S_CONTINUE
-      command = STDIN.readline.strip.downcase
+      command = get_char
 
       case command
       when S_GAME_ID
@@ -69,7 +73,7 @@ class PatUnEvent
     choices = MAIN_MENU_CHOICES.inject([]){|a,(desc,ch)| a << "#{ch}=#{desc}" unless omits.include?(desc); a}
     while true
       printf "Enter command (%s): ", choices.join(", ")
-      command = STDIN.readline.strip.downcase
+      command = get_char
 
       # Do not respond to commands in the omits-list
       choice_pair = MAIN_MENU_CHOICES.rassoc(command)
@@ -87,13 +91,38 @@ class PatUnEvent
       when S_MORE
         event = get_more
         return event if event
+
       when S_NEW_GAME
-        return {:event => :event_new_game}
+        msg = "Are you sure you want to start a new game? [y/n] "
+        is_confirmed(msg) ? (return {:event => :event_new_game}) : next
+
       when S_QUIT
-        return {:event => :event_quit}
+        msg = "Are you sure you want to quit? [y/n] "
+        is_confirmed(msg) ? (return {:event => :event_quit}) : next
       end
 
     end
+  end
+
+  ############################################################################
+  # Prompts with a confirmation message. Returns true if confirmed, else returns false.
+  def is_confirmed(msg, s_true="y", s_false="n")
+    printf "%s", msg
+    get_char == s_true
+  end
+
+  ############################################################################
+  # We want a single character from STDIN (but string length *might* not be 1)
+  def get_char
+    if WITHOUT_ENTER_KEY
+      ch = STDIN.getch
+      puts ch
+      exit if ch.ord == 3		# Ctl-C to quit
+
+    else
+      ch = STDIN.readline
+    end
+    ch.strip.downcase
   end
 
 end
